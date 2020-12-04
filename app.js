6f80ee7830fdd2e99fc5d46c0fc0b1ccd69d54d1
Onur AKSAR <onuraksar96@gmail.com>
2020-12-04 23:16:30 +0300

    Add Difficulty Levels

--- app.js
+++ app.js
@@ -6,18 +6,64 @@ document.addEventListener('DOMContentLoaded', function() {
   const gameBoard = document.querySelector(".game-board");
   const button = document.querySelector(".btn-create");
 
+  // Choosing the Level for the Game:
+  const levelMenu = document.querySelector("#level_menu");
+
+  let selectedOption = levelMenu.value;
+
+
+  const allColors = ["red", "blue", "green", "yellow", "pink", "magenta"];
+
+  let colors = [];
+
+  levelMenu.addEventListener("change", function() {
+    selectedOption = this.value;
+    colors = []; // clear the array content to prevent overriding when user changes the option everytime.
+    switch (selectedOption) {
+      case "easy": // choose 4 random colors
+        chooseRandomColors(4);
+        break;
+      case "medium":
+        chooseRandomColors(5);
+        break;
+      case "hard":
+        chooseRandomColors(6);
+        break;
+      default:
+        chooseRandomColors(4);
+    }
+
+
+  });
+
+
+  // if selectedOption = easy. Choose random 4 colors from allColors
+  // if selectedOption = medium. Choose ramdom 5 colors from allColors
+  // if selectedOption = hard. Use allColors array directly..
+
+
+  // function that chooses random colors from allColors, using the number parameters:
+
+  function chooseRandomColors(numberOfCards) {
+    // choose 4 random index numbers
+    let tempAllColors = []; // Create a temporary array.
+    allColors.forEach(item => {
+      tempAllColors.push(item)
+    }); // duplice the allColors array as tempAllColors.
+
+    shuffle(tempAllColors); // shuffle the array to choose random colors.
+
+    for (let i = 0; i < numberOfCards; i++) {
+      colors.push(tempAllColors[i]);
+    }
+  }
+
+
   button.addEventListener("click", createBoard);
 
-  const colors = ["red", "blue", "green"];
+
 
   let pickedColors = [];
-  let tempPickedColors = []; // will be used inside the function below.
-  for (i = 0; i < colors.length; i++) {
-    tempPickedColors.push(colors[i]);
-    tempPickedColors.push(colors[i]); // duplicate the elements inside the array.
-    // ["red", "red", "blue", "blue", "green", "green"]
-  }
-  pickedColors = tempPickedColors; // Equalize the arrays.
 
 
   let firstCard;
@@ -26,7 +72,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let firstCardColor;
   let secondCardColor;
 
-// Shuffle the cards so that the order of the color will be different.
+  // Shuffle the cards so that the order of the color will be different.
   function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * i);
@@ -36,7 +82,26 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
+  // function that creates the Board:
+
+
   function createBoard() {
+
+    let pickedColors = [];
+
+    for (let i = 0; i < colors.length; i++) {
+      // tempPickedColors.push(colors[i]);
+      // tempPickedColors.push(colors[i]); // duplicate the elements inside the array.
+
+      pickedColors.push(colors[i]);
+      pickedColors.push(colors[i]); // duplicate the elements inside the array.
+      // ["red", "red", "blue", "blue", "green", "green"]
+
+    }
+
+    pickedColors.forEach(item => {
+
+    })
 
     shuffle(pickedColors); // shuffle the cards.
     // First, check if a set of cards already exist. IF they do, delete them all and replace with new ones.
@@ -54,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
       document.querySelectorAll(".card").forEach(item => {
-        item.addEventListener("click", handleClick);  // add event listener for all the cards on the board.
+        item.addEventListener("click", handleClick); // add event listener for all the cards on the board.
       });
 
 
@@ -65,25 +130,21 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
 
           // check if any card has been clicked and is up already.
-          console.log(this);
+
           const numberOfOnCards = CardsOn(); // returns true if all the cards are off.
 
           // if a card is already flipped:
           if (numberOfOnCards === 0) {
             this.classList.toggle("flipped-over"); // if none or one card is already on, you can flip over another card.
-            console.log(this);
             firstCardColor = this.style.backgroundColor; // keep the first card's color in a variable.
             firstCard = this;
-            console.log(firstCardColor);
 
           } else if (numberOfOnCards === 1) {
             this.classList.toggle("flipped-over"); // if none or one card is already on, you can flip over another card.
             secondCardColor = this.style.backgroundColor;
             secondCard = this;
-            console.log(secondCardColor);
             setTimeout(checkSameCards, 1000); // if they are equal change their color ( by adding a css class.)
           } else {
-            console.log("enough of with the cards!!");
 
           }
 
@@ -127,14 +188,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function checkSameCards() {
     if (firstCard !== secondCard && firstCardColor === secondCardColor) {
-      console.log("they are equal!!");
       // firstCard.classList.remove("flipped-over");
       firstCard.classList.add("matched");
       secondCard.classList.add("matched");
       // secondCard.classList.remove("flipped-over");
 
     } else {
-      console.log("they are not the same!");
       // if they are not the same cards. Flip them back off
       firstCard.classList.toggle("flipped-over");
       secondCard.classList.toggle("flipped-over");
@@ -143,9 +202,6 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
 
-  setTimeout(function() {
-    console.log()
-  }, 6000);
 
 
 });
